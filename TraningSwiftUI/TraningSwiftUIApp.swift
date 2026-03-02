d1b79e8270c9ca222da7990923dc32eeef5f6010
FDCSIGFRID <fdc.sigfrid@gmail.com>
2026-03-02 10:05:29 +0800

    Initial Commit

---
+++ TraningSwiftUI/TraningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraningSwiftUIApp.swift
+//  TraningSwiftUI
+//
+//  Created by Sigfri on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TraningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
